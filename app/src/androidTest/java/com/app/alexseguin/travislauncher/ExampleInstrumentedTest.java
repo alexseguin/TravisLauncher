b3471054708728f621bc8a5e8a2790f5c5090da6
Alexandre Douglas Séguin <asegu103@uottawa.ca>
2019-01-02 03:07:04 -0500

    added unit test for Volley API calls

--- app/src/androidTest/java/com/app/alexseguin/travislauncher/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/alexseguin/travislauncher/ExampleInstrumentedTest.java
@@ -4,10 +4,19 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,9 +27,49 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
+
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.app.alexseguin.travislauncher", appContext.getPackageName());
     }
+    class ResponseResult {
+        String result;
+    }
+
+    @Test
+    public void testAPICall() throws InterruptedException {
+
+        final ResponseResult res = new ResponseResult();
+
+        await().atMost(5, SECONDS).until(() -> {
+            // Context of the app under test.
+            Context appContext = InstrumentationRegistry.getTargetContext();
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(appContext);
+            String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            res.result = response;
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    assert(false);
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+            return true;
+        });
+        System.out.println(res.result);
+        assert(res != null);
+    }
+
+
 }
